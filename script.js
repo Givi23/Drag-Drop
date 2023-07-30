f1b17b791d537d4aec684298040a40a57ac873de
Givi23 <givi023@mail.ru>
2023-07-30 22:38:37 +0300

    30.07.23

---
+++ script.js
@@ -0,0 +1,6 @@
+const item = document.querySelector('.item');
+console.log(item);
+const placeholders = document.querySelectorAll('.placeholder');
+console.log(placeholders); 
+const body = document.querySelector('.body');
+console.log(body);
