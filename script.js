5b919bf541822c1bcfda294401b4a533f183b0ae
Givi23 <givi023@mail.ru>
2023-08-01 22:01:49 +0300

    01.08.23

--- script.js
+++ script.js
@@ -2,8 +2,101 @@ const item = document.querySelector('.item');
 console.log(item);
 const placeholders = document.querySelectorAll('.placeholder');
 console.log(placeholders); 
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
 console.log(body);
+// (первое событие, ссылка на функцию)
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend) 
+
+// имеем возможность управлять ячейкой (placeholder) отдельно на каждую
+// цикл (создал переменную, совершить обход по массиву-(маршрут))
+for(const placeholder of placeholders){
+    console.log(placeholder); // при каждом повторении цикла в переменную placeholder присваеваеться значения элемента массива
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
+} 
+
+function dragover(event){
+    console.log('dragover');
+}
+function dragenter(event){
+    console.log('dragenter');
+}
+function dragleave(event){
+    console.log('dragleave');
+}
+function drop(event){
+    console.log('drop');
+}
+
+
+
+function dragstart(event){
+    console.log('dragstart');
+}
+function dragend(event){
+    console.log('dragend');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let fruits = ["яблоко", "апельсин", "груша"];
 // alert (fruits);
@@ -74,3 +167,18 @@ console.log(body);
 // }
 // let result = minus(c,d);
 // alert(result);
+
+// функция с использованием параметров
+// function checkAge(age){
+//     if(age >= 18){
+//         return true;
+//     } else {
+//         return confirm('А родители разрешили?');
+//     }
+// }
+// let age = prompt('Cколько вам лет?', 18);
+// if (checkAge (age)){
+//     alert('Доступ получен');
+// } else {
+//     alert('Доступ закрыт');
+// }
